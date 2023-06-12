a0dd60a405c8123331bf61e313020094f082bece
jpclrocha <jpclrocha@gmail.com>
2023-06-11 21:43:01 -0300

    first commit

---
+++ src/main/java/com/jopezin/workshopmongodb/WorkshopJavaSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.jopezin.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopJavaSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopJavaSpringMongoDbApplication.class, args);
+	}
+
+}
