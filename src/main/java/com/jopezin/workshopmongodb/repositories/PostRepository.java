cf9c5109837b06602f37d25392399df61e4771c8
jpclrocha <jpclrocha@gmail.com>
2023-06-19 20:32:33 -0300

    spring query methods

--- src/main/java/com/jopezin/workshopmongodb/repositories/PostRepository.java
+++ src/main/java/com/jopezin/workshopmongodb/repositories/PostRepository.java
@@ -3,5 +3,8 @@ package com.jopezin.workshopmongodb.repositories;
 import com.jopezin.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
