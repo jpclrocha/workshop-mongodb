42061a87405de26b6296c2b00d0f88f652c498ac
jpclrocha <jpclrocha@gmail.com>
2023-06-19 11:58:02 -0300

    Post findById endpoint

---
+++ src/main/java/com/jopezin/workshopmongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.jopezin.workshopmongodb.services;
+
+import com.jopezin.workshopmongodb.domain.Post;
+import com.jopezin.workshopmongodb.exceptions.ObjectNotFoundException;
+import com.jopezin.workshopmongodb.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado!"));
+    }
+}
