11b8e6eef0dddfcb632fbd78b01a9c2b9ed06b82
jpclrocha <jpclrocha@gmail.com>
2023-06-19 09:09:10 -0300

    Author DTO

--- src/main/java/com/jopezin/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/jopezin/workshopmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.jopezin.workshopmongodb.config;
 
 import com.jopezin.workshopmongodb.domain.Post;
 import com.jopezin.workshopmongodb.domain.User;
+import com.jopezin.workshopmongodb.dto.AuthorDTO;
 import com.jopezin.workshopmongodb.repositories.PostRepository;
 import com.jopezin.workshopmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,11 +36,11 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, LocalDate.from(dtf.parse("21/03/2018")),"Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", maria);
-        Post post2 = new Post(null, LocalDate.from(dtf.parse("23/03/2018")),"Bom dia", "Acordei feliz hoje!",maria);
+        Post post1 = new Post(null, LocalDate.from(dtf.parse("21/03/2018")),"Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
+        Post post2 = new Post(null, LocalDate.from(dtf.parse("23/03/2018")),"Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
     }
 }
